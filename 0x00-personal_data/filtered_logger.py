b95b388cceccdf7ce59cc2d70cf9f7b1afd507b5
mwangii <jlmwangi254@gmail.com>
2024-10-08 17:38:44 +0300

    fix issues

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,10 +3,11 @@
 
 
 import re
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    #  for field in fields:
-     #   pattern = r"({}=)([^{}]+)".format(re.escape(field), re.escape(separator))
-     #   message = re.sub(pattern, r"\1" + redaction, message)
-    return re.sub(r'({})=([^{}]+)'.format('|'.join(map(re.escape, fields)), re.escape(separator)), r'\1={}'.format(redaction), message)
+    for field in fields:
+        pattern = r"({}=)([^{}]+)".format(re.escape(field), re.escape(separator))
+        message = re.sub(pattern, r"\1" + redaction, message)
+    return message
