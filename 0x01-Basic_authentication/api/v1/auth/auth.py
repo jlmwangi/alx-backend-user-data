56372287082cb6fbb77d9e13daea8c52a844cc18
mwangii <jlmwangi254@gmail.com>
2024-10-16 17:20:22 +0300

    require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,20 @@ from typing import List, TypeVar
 class Auth:
     '''class to manage the API authentication'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        '''returns False, not required now'''
-        return False
+        '''Define which routes dont need authentication'''
+        if path is None:
+            return True
+        normalized_path = path.rstrip('/')  # remove trailing slashes
+        excluded_path_list = []
+
+        #  normalize each excluded path
+        for excluded_path in excluded_paths:
+            normalized_excluded_path = excluded_path.rstrip('/')  # remove
+            excluded_path_list.append(normalized_excluded_path)
+
+        if normalized_path in excluded_path_list:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''returns None'''
