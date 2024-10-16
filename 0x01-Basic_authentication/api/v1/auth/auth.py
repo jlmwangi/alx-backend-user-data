86da7dbb5770a90f4e19e1f2d2af26b82e30fe79
mwangii <jlmwangi254@gmail.com>
2024-10-16 15:51:19 +0300

    task_3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+from flask import request
+from models.user import User
+from typing import List, TypeVar
+
+
+class Auth:
+    '''class to manage the API authentication'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''returns False, not required now'''
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        '''returns None'''
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''returns None'''
+        return None
