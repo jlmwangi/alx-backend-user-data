e3463a733e619c6c4358933c8ee440b4167e50f9
mwangii <jlmwangi254@gmail.com>
2024-10-17 07:23:41 +0300

    task_5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,29 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+auth_type = os.getenv('AUTH_TYPE')
+if auth_type:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def filter_request():
+    '''filter requests to know which requires authen and which doesnt'''
+    path_list = ['/api/v1/status/',
+                 '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth is None:
+        return
+    # if request path doesnt require authenticaton do nothing
+    if not auth.require_auth(request.path, path_list):
+        return
+    # if auth header is missing
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
