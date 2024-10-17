5c7add74f9f951688ef00090303f34a3e80f4671
mwangii <jlmwangi254@gmail.com>
2024-10-17 13:48:20 +0300

    import binascii

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ from api.v1.auth.auth import Auth
 from flask import request
 from models.user import User
 from typing import List, TypeVar
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -21,3 +23,20 @@ class BasicAuth(Auth):
 
         result = authorization_header.split(" ")
         return result[-1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        '''returns the decoded value of a base64 string'''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            '''check for validity'''
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+
+        except (binascii.Error, UnicodeDecodeError):
+            return None
