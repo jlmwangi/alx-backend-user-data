e118f79142996d5c46140221538a70a29bbb1ec4
mwangii <jlmwangi254@gmail.com>
2024-10-19 13:25:25 +0300

    task_2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,8 +7,18 @@ from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+import uuid
 
 
 class SessionAuth(Auth):
     '''a class that inherits from auth'''
-    pass
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) ->str:
+        '''creates a session id for a user_id'''
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        #  generate unique session id
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
