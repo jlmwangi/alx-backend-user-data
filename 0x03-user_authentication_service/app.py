17a3604bf2328d411f00ea4d43bef8ba33b3e3cc
mwangii <jlmwangi254@gmail.com>
2024-10-24 15:40:39 +0300

    create a flask appp

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,10 +5,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def get_route():
     '''create a flask app with a get route'''
     return jsonify(message="Bienvenue")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
